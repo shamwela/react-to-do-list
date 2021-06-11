dfc3077d4076c38f4cc67324623a211d8eb0720e
Sha Mwe La <shamwela2002@gmail.com>
2021-06-11 20:29:02 +0630

    Create tasks component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import TaskInput from './components/taskInput';
+import Tasks from './components/tasks';
 
 function App() {
   return (
     <div className="App">
       <h1>React To-do List</h1>
       <TaskInput />
+      <Tasks />
     </div>
   );
 }
---
+++ src/components/tasks.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Task from './task';
+
+class Tasks extends Component {
+  state = {};
+  render() {
+    return <Task />;
+  }
+}
+
+export default Tasks;
